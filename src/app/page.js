6a49491a014ad19b9b44c3ac4028edf189f5ba89
Jinali sanghvi <jinali.sanghvi@krishtechnolabs.com>
2024-12-20 15:43:38 +0530

    added git ignore

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import Footer from "@/components/Footer/page";
+import Header from "@/components/Header/page";
+
+export default function Home() {
+  return (
+    <div>
+     hello
+    {/* <Footer /> */}
+    <Header />
+    </div>
+  );
+}
---
+++ src/components/Header/page.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <div>
+            shop all 
+        </div>
+        <div>
+            kitchen  
+        </div>
+        <div>
+            About
+        </div>
+        <div>
+            Contact
+        </div>
+    </div>
+  )
+}
+
+export default Header
